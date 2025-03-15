288e321691174b14ec2ae0c1187450d916c77409
muhammed yolcu <sefa.yolcu2@gmail.com>
2025-03-15 11:54:21 +0300

    widget testi eklendi

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,29 +1,32 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_tech_task/features/main_page/view/main_page_screen.dart';
+import 'package:flutter_tech_task/features/main_page/view_model/main_page_cubit.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:easy_localization/easy_localization.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-    // Build our app and trigger a frame.
-    await tester.pumpWidget(const MainPageScreen());
+  testWidgets('Testing for MainPageScreen', (WidgetTester tester) async {
+    await EasyLocalization.ensureInitialized();
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    await tester.pumpWidget(
+      EasyLocalization(
+        supportedLocales: [Locale('en'), Locale('tr')],
+        path: 'assets/translations',
+        fallbackLocale: Locale('en'),
+        child: BlocProvider(
+          create: (context) => MainPageCubit(),
+          child: MainPageScreen(
+            changeThemeMode: () => ThemeData.dark(),
+          ),
+        ),
+      ),
+    );
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    if (find.byType(ListView).evaluate().isNotEmpty) {
+      //MainPageScreen ekranında ListView olup olmadığını kontrol ediyor. (byType Widget türünü belirliyor,evaluate widget varlığını kontrol ediyor. )
+      await tester.fling(find.byType(ListView), const Offset(0, 500), 1000); // Aşağı doğru kaydır
+      await tester.pumpAndSettle(); //Animasyonlar bitinceye kadr bekle
+    }
   });
 }
